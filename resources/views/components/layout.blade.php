c4f358d2204566c09a347238182c30ca7ebb2abe
Kristopher-Code <knicholcode@gmail.com>
2024-05-30 16:16:59 +0100

    First commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,7 +24,7 @@
                 </a>
             </div>
             <div class="space-x-6 font-bold">
-                <a href="/">Job</a>
+                <a href="/">Jobs</a>
                 <a href="#">Careers</a>
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
